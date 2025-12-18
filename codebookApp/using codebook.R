aef2ba71ebcb28dceea3b22a4849af17e9942ac0
Ethan O. Kile <eok5206@psu.edu>
2025-12-18 13:47:35 -0500

    Spring Cleaning

---
+++ codebookApp/using codebook.R
@@ -0,0 +1,15 @@
+# Set wd, load all and clear environment
+library(rstudioapi)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rm(list = ls())
+devtools::load_all(recompile = FALSE)
+devtools::document()
+
+# Pull data from api
+data <- getStudyData(study_ID = "1045")
+
+# Save it to processedData folder in Codebook_RMD
+saveData(data)
+
+# Generate codebook
+generateCodebook()
