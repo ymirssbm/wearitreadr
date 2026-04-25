801003f3aa4980250ceb03c985d391f9b450ba23
Ethan O. Kile <eok5206@psu.edu>
2026-04-25 14:44:37 -0400

    Big clean up of uneeded files

--- ai codebooks/save ai data.R
+++
@@ -1,9 +0,0 @@
-source("~/R/Tim Lab/GIT R directory/wearitreadr/R/ProcessData.R")
-
-
-# Write each data frame in 'data' to a CSV file named after its name
-write.csv(studyData$questionMap, "questionMap.csv", row.names = FALSE)
-write.csv(studyData$responseMap, "responseMap.csv", row.names = FALSE)
-write.csv(studyData$surveyData, "surveyData.csv", row.names = FALSE)
-write.csv(studyData$surveyCombined, "surveyCombined.csv", row.names = FALSE)
-
--- codebookApp/Codebook_RMD/Data/modify data.R
+++
@@ -1,6 +0,0 @@
-df <- read.csv("blockMap.csv")
-
-df$Question.Type.Display.Name[44,] <- "Free Response"
-
-
-write.csv(df,"blockMap.csv", row.names = FALSE)
